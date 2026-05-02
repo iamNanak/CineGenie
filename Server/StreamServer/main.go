cd1f21ecc01f5dbe1f08aeb124722a91ba0499cf
iamNanak <iamnanak01@gmail.com>
2026-05-03 01:41:13 +0530

    Add initial server setup, database connection, and movie controller functionality

---
+++ Server/StreamServer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	controllers "github.com/iamNanak/CineGenie/Server/StreamServer/controllers"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, ("hello CineGenie"))
+	})
+
+	router.GET("/movies", controllers.GetMovies())
+
+	router.GET("/movie/:imdb_id", controllers.GetMovieByID())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
+
+}
